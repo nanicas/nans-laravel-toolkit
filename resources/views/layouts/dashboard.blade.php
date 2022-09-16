8d98a0f1d042d84542eeefc49afc8389e6cd2fa2
José Victor <josevictor.jvds@gmail.com>
2022-09-16 17:10:52 -0300

    ajustes

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -129,7 +129,7 @@
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">
         {{ $session_data['user']['name'] }}
-        @yield('slot-user-name')
+        @yield('user-name-complement')
     </a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
