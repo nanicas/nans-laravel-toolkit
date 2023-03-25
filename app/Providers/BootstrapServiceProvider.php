677a65e035880039d0d6231e1653d216119918c6
José Victor <josevictor.jvds@gmail.com>
2023-03-25 11:54:16 -0300

    Atualizando e removendo responsabilidades

--- app/Providers/BootstrapServiceProvider.php
+++ app/Providers/BootstrapServiceProvider.php
@@ -4,8 +4,6 @@ namespace Zevitagem\LaravelToolkit\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\Blade;
-use Zevitagem\LaravelToolkit\View\Components\DynamicEntityComponent;
 
 class BootstrapServiceProvider extends ServiceProvider
 {
@@ -28,38 +26,10 @@ class BootstrapServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         
-        Blade::component('package-dynamic-entity-component', DynamicEntityComponent::class);
-        
         $src = __DIR__ . '/../..';
-        
-        $this->publishes([
-            $src.'/resources/css' => resource_path('template/css'),
-        ], 'template_core:resources');
-        
-        $this->publishes([
-            $src.'/resources/js' => resource_path('template/js'),
-        ], 'template_core:resources');
-        
-        $this->publishes([
-            $src.'/resources/sass' => resource_path('template/sass'),
-        ], 'template_core:resources');
-        
-        $this->publishes([
-            $src.'/resources/views' => resource_path('views/vendor/template'),
-        ], 'template_core:views');
-        
-        $this->publishes([
-            $src . '/public' => public_path('template')
-        ], 'template_core:public');
-        
-        $this->publishes([
-            $src.'/routes' => base_path('routes'),
-        ], 'template_core:routes');
-        
+
         $this->publishes([
             $src.'/config' => config_path(),
         ], 'template_core:config');
-        
-        $this->loadViewsFrom(resource_path('views/vendor/template'), 'template_core');
     }
 }
